22504a0f9478e2fd4a2658803456f0ac588d53c9
Krystian Woźniak <krys.blu@gmail.com>
2019-02-27 20:08:44 +0100

    Added navigation among different components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,9 @@ import {FormsModule} from "@angular/forms";
 import {AppComponent} from './app.component';
 import {HeroesComponent} from './heroes/heroes.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {MessagesComponent} from './messages/messages.component';
+import {AppRoutingModule} from './app-routing.module';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 
 @NgModule({
